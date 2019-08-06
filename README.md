4b63fb959ad41d0af87e87e4e76a6b19a8c6b597
Pranesh.g <praneshpsg239@gmail.com>
2019-08-06 20:15:17 +0530

    Initial setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Component Patterns 🎲
+
+### 🚨 Each branch in this repo carries a pattern 🚨
+
+<img src="https://octodex.github.com/images/andycat.jpg" alt="alt text" width="300">
+
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
---
+++ src/Components/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./styles.scss";
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <h1>
+        <span role="img" aria-label="emoji">
+          🚨
+        </span>
+        Please check the branches for Patterns
+        <span role="img" aria-label="emoji">
+          🚨
+        </span>
+      </h1>
+    );
+  }
+}
+
+export default App;
